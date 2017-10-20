2c36a59de05da8688689ef0fe7d98e3eff094403
Luca Weiss <luca@z3ntu.xyz>
2017-10-20 19:35:53 +0200

    librazer: Add color to method check script

--- src/librazer/check_if_methods_implemented.sh
+++ src/librazer/check_if_methods_implemented.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #set -x
-daemon_source=/mnt/hdd/razer-drivers/daemon
+
+# colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
+daemon_source=/mnt/hdd/razer/openrazer/daemon
 
 declare -a endpoints
 IFS=$'\n'
@@ -9,16 +16,16 @@ endpoints=($(grep -rh "@endpoint" $daemon_source | cut -d"'" -f2,4 | sed "s/'/\"
 # TODO Somehow make entries in $endpoints unique
 
 for method in "${endpoints[@]}"; do
-  echo "Searching for: $method ..."
+  echo -e "Searching for: ${BLUE}$method${NC} ..."
   method_grep=$(grep "$method" librazer.cpp)
   if [ ! -z $(echo $method_grep | grep "capabilities.insert") ]; then
-    echo "Capabilities: ok"
+    echo -e "Capabilities: ${GREEN}ok${NC}"
   else
-    echo "Capabilities: no"
+    echo -e "Capabilities: ${RED}no${NC}"
   fi
   if [ ! -z $(echo $method_grep | grep -E "prepareDeviceQDBusMessage|prepareGeneralQDBusMessage") ]; then
-    echo "Implementation: ok"
+    echo -e "Implementation: ${GREEN}ok${NC}"
   else
-    echo "Implementation: no"
+    echo -e "Implementation: ${RED}no${NC}"
   fi
 done
