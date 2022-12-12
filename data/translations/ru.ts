3d942f74ddbf44ba77d4a3899646f80a58bb69c9
Alexey Minnekhanov <alexey.min@gmail.com>
2022-12-12 16:27:46 +0300

    Add Russian translation

---
+++ data/translations/ru.ts
@@ -0,0 +1,394 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>CustomEditor</name>
+    <message>
+        <location filename="../../src/customeditor/customeditor.cpp" line="18"/>
+        <source>RazerGenie - Custom Editor</source>
+        <translation>RazerGenie - Редактор</translation>
+    </message>
+    <message>
+        <location filename="../../src/customeditor/customeditor.cpp" line="90"/>
+        <source>Set</source>
+        <translation>Задать</translation>
+    </message>
+    <message>
+        <location filename="../../src/customeditor/customeditor.cpp" line="91"/>
+        <source>Clear</source>
+        <translation>Очистить</translation>
+    </message>
+    <message>
+        <location filename="../../src/customeditor/customeditor.cpp" line="92"/>
+        <source>Clear All</source>
+        <translation>Очистить все</translation>
+    </message>
+    <message>
+        <location filename="../../src/customeditor/customeditor.cpp" line="137"/>
+        <source>You are using a keyboard with a layout which is not known to the daemon. Please help us by visiting &lt;a href=&apos;https://github.com/openrazer/openrazer/wiki/Keyboard-layouts&apos;&gt;https://github.com/openrazer/openrazer/wiki/Keyboard-layouts&lt;/a&gt;. Using a fallback layout for now.</source>
+        <translation>Вы используете клавиатуру с раскладкой, неизвестной сервису OpenRazer. Пожалуйста, помогите нам это исправить, посетив &lt;a href=&apos;https://github.com/openrazer/openrazer/wiki/Keyboard-layouts&apos;&gt;https://github.com/openrazer/openrazer/wiki/Keyboard-layouts&lt;/a&gt;. Будет использованна резервная раскладка.</translation>
+    </message>
+    <message>
+        <location filename="../../src/customeditor/customeditor.cpp" line="304"/>
+        <source>The file %1.json, used for the custom editor failed to load: %2</source>
+        <translation>Файл %1.json, нужный редактору, не удалось загрузить: %2</translation>
+    </message>
+    <message>
+        <location filename="../../src/customeditor/customeditor.cpp" line="324"/>
+        <source>Error updating the lighting data.</source>
+        <translation>Ошибка при обновлении подсветки.</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceListWidget</name>
+    <message>
+        <location filename="../../src/devicelistwidget.cpp" line="28"/>
+        <source>Downloading image...</source>
+        <translation>Загрузка изображения...</translation>
+    </message>
+    <message>
+        <location filename="../../src/devicelistwidget.cpp" line="68"/>
+        <source>No image</source>
+        <translation>Нет изображения</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceWidget</name>
+    <message>
+        <location filename="../../src/devicewidget.cpp" line="45"/>
+        <source>Lighting</source>
+        <translation>Подсветка</translation>
+    </message>
+    <message>
+        <location filename="../../src/devicewidget.cpp" line="66"/>
+        <source>Polling rate</source>
+        <translation>Частота опроса</translation>
+    </message>
+    <message>
+        <location filename="../../src/devicewidget.cpp" line="97"/>
+        <source>Open custom editor</source>
+        <translation>Открыть редактор</translation>
+    </message>
+    <message>
+        <location filename="../../src/devicewidget.cpp" line="129"/>
+        <source>Serial number: %1</source>
+        <translation>Серийный номер: %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/devicewidget.cpp" line="132"/>
+        <source>Firmware version: %1</source>
+        <translation>Версия прошивки: %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/devicewidget.cpp" line="150"/>
+        <source>Failed to set polling rate</source>
+        <translation>Не удалось установить частоту опроса</translation>
+    </message>
+</context>
+<context>
+    <name>DpiComboBoxWidget</name>
+    <message>
+        <location filename="../../src/dpicomboboxwidget.cpp" line="23"/>
+        <source>DPI</source>
+        <translation>DPI</translation>
+    </message>
+    <message>
+        <location filename="../../src/dpicomboboxwidget.cpp" line="53"/>
+        <source>Failed to set DPI</source>
+        <translation>Не удалось установить разрешение</translation>
+    </message>
+</context>
+<context>
+    <name>DpiSliderWidget</name>
+    <message>
+        <location filename="../../src/dpisliderwidget.cpp" line="34"/>
+        <source>DPI</source>
+        <translation>DPI</translation>
+    </message>
+    <message>
+        <location filename="../../src/dpisliderwidget.cpp" line="38"/>
+        <source>Lock X/Y</source>
+        <translation>Закрепить X/Y</translation>
+    </message>
+    <message>
+        <location filename="../../src/dpisliderwidget.cpp" line="47"/>
+        <source>DPI X</source>
+        <translation>DPI X</translation>
+    </message>
+    <message>
+        <location filename="../../src/dpisliderwidget.cpp" line="48"/>
+        <source>DPI Y</source>
+        <translation>DPI Y</translation>
+    </message>
+    <message>
+        <location filename="../../src/dpisliderwidget.cpp" line="161"/>
+        <source>Failed to set dpi</source>
+        <translation>Не удалось установить разрешение</translation>
+    </message>
+</context>
+<context>
+    <name>LedWidget</name>
+    <message>
+        <location filename="../../src/ledwidget.cpp" line="24"/>
+        <source>Lighting %1</source>
+        <translation>Подсветка %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/ledwidget.cpp" line="68"/>
+        <source>Brightness</source>
+        <translation>Яркость</translation>
+    </message>
+    <message>
+        <location filename="../../src/ledwidget.cpp" line="114"/>
+        <source>Left</source>
+        <translation>Налево</translation>
+    </message>
+    <message>
+        <location filename="../../src/ledwidget.cpp" line="116"/>
+        <source>Right</source>
+        <translation>Направо</translation>
+    </message>
+    <message>
+        <location filename="../../src/ledwidget.cpp" line="215"/>
+        <source>Failed to change brightness</source>
+        <translation>Не удалось сменить яркость</translation>
+    </message>
+    <message>
+        <location filename="../../src/ledwidget.cpp" line="283"/>
+        <source>Failed to change effect</source>
+        <translation>Не удалось сменить эффект</translation>
+    </message>
+</context>
+<context>
+    <name>Preferences</name>
+    <message>
+        <location filename="../../src/preferences/preferences.cpp" line="18"/>
+        <location filename="../../src/preferences/preferences.cpp" line="89"/>
+        <source>RazerGenie - Preferences</source>
+        <translation>RazerGenie - Настройки</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferences/preferences.cpp" line="25"/>
+        <source>About:</source>
+        <translation>О программе:</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferences/preferences.cpp" line="29"/>
+        <source>RazerGenie Version: %1</source>
+        <translation>Версия RazerGenie: %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferences/preferences.cpp" line="38"/>
+        <source>OpenRazer Daemon Version: %1</source>
+        <translation>Версия демона OpenRazer: %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferences/preferences.cpp" line="41"/>
+        <source>General:</source>
+        <translation>Основные:</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferences/preferences.cpp" line="45"/>
+        <source>For displaying device images, RazerGenie downloads the image behind the URL specified for a device in the OpenRazer daemon source code. This will only be done for devices that are connected to the PC and only once, as the images are cached locally. For reviewing, what information Razer might collect with these connections, please consult the Razer Privacy Policy (https://www.razer.com/legal/privacy-policy).</source>
+        <translation>Для показа изображений устройств, RazerGenie загрузит изображение по ссылке, указанной в коде демона OpenRazer. Это будет сделано только для подключенных к ПК устройств и только один раз (картинки будут сохранены на локальном диске). Компания Razer может собирать информацию об этих соединениях. Для точной информации посетите страницу https://www.razer.com/legal/privacy-policy.</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferences/preferences.cpp" line="49"/>
+        <source>Download device images</source>
+        <translation>Загружать изображения устройств</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferences/preferences.cpp" line="58"/>
+        <source>Daemon backend to use:</source>
+        <translation>Бэкенд демона:</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferences/preferences.cpp" line="68"/>
+        <source>Please restart the application for the switch to take effect.</source>
+        <translation>Пжалуйста, перезапустите приложение для того, чтобы изменения вступили в силу.</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <location filename="../../src/util.cpp" line="12"/>
+        <source>Error!</source>
+        <translation>Ошибка!</translation>
+    </message>
+    <message>
+        <location filename="../../src/util.cpp" line="19"/>
+        <source>Information!</source>
+        <translation>Информация!</translation>
+    </message>
+</context>
+<context>
+    <name>RazerGenie</name>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="51"/>
+        <source>The OpenRazer daemon is not installed</source>
+        <translation>Демон OpenRazer не установлен</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="52"/>
+        <source>The daemon is not installed or the version installed is too old. Please follow the installation instructions on the website!
+
+If you are running RazerGenie as a flatpak, you will still have to install OpenRazer outside of flatpak from a distribution package.</source>
+        <translation>Демон не установлени или версия слишком устарела. Подалуйста следуйте инструкциям по установке на сайте!
+
+Если вы используете RazerGenie из flatpak, всё равно необходимо установить OpenRazer за пределами flatpak из пакетов вашего дистрибутива.</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="53"/>
+        <source>Open OpenRazer website</source>
+        <translation>Открыть веб-сайт OpenRazer</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="55"/>
+        <location filename="../../src/razergenie.cpp" line="71"/>
+        <location filename="../../src/razergenie.cpp" line="89"/>
+        <source>Open settings</source>
+        <translation>Открыть настройки</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="69"/>
+        <source>The OpenRazer daemon is not available.</source>
+        <translation>Демон OpenRazer недоступен.</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="70"/>
+        <source>The OpenRazer daemon is not started and you are not using systemd as your init system.
+You have to either start the daemon manually every time you log in or set up another method of autostarting the daemon.
+
+Please consult the documentation for details.</source>
+        <translation>Демон OpenRazer не запущен и вы не используете systemd.
+Необходимо или запускать демон вручную при каждом входе в систему, или использовать какой-то другой метод для его автозапуска.
+
+Подробнее см. в документации.</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="84"/>
+        <source>The OpenRazer daemon is currently not available. The status output is below.</source>
+        <translation>Демон OpenRazer сейчас недоступен. Ниже вывод статуса.</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="86"/>
+        <source>If you think, there&apos;s a bug, you can report an issue on GitHub:</source>
+        <translation>Если вам кажется, что вы нашли баг, репортите его на GitHub:</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="87"/>
+        <location filename="../../src/razergenie.cpp" line="404"/>
+        <location filename="../../src/razergenie.cpp" line="412"/>
+        <source>Report issue</source>
+        <translation>Сообщить об ошибке</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="110"/>
+        <source>The OpenRazer daemon is not set to auto-start. Click &quot;Enable&quot; to use the full potential of the daemon right after login.</source>
+        <translation>Демон OpenRazer не установлен на автозапуск. Нажмите &quot;Включить&quot; чтобы задействовать полный потенциал демона сразу после входа в систему.</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="111"/>
+        <source>Enable</source>
+        <translation>Включить</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="144"/>
+        <source>Daemon version: %1</source>
+        <translation>Версия демона: %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="164"/>
+        <source>The D-Bus connection was re-established.</source>
+        <translation>Соединение с D-Bus было пере-установлено.</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="172"/>
+        <source>The D-Bus connection was lost, which probably means that the daemon has crashed.</source>
+        <translation>Свзять с демоном по D-Bus утеряна, возможно потому что демон упал.</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="400"/>
+        <source>No device was detected</source>
+        <translation>Устройства не обнаружены</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="401"/>
+        <source>The OpenRazer daemon didn&apos;t detect a device that is supported.
+This could also be caused due to a misconfiguration of this PC.</source>
+        <translation>Демон OpenRazer не обнаружил поддерживаемое устройство.
+Это также могло быть вызвано неверными настрйоками данного ПК.</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="402"/>
+        <source>Open supported devices</source>
+        <translation>Открыть поддерживаемые устройства</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="407"/>
+        <source>The daemon didn&apos;t detect a device that is connected</source>
+        <translation>Демон не обнаружил подключенное устройство</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="408"/>
+        <source>Linux detected connected devices but the daemon didn&apos;t. This could be either due to a permission problem or a kernel module problem.</source>
+        <translation>Linux обнаружил устройства, но демон не смог. Это может быть из-за неверных разрешений или проблема модулей ядра.</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="410"/>
+        <source>Open troubleshooting page</source>
+        <translation>Открыть страницу &quot;Исправление проблем&quot;</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="431"/>
+        <source>Error while syncing devices.</source>
+        <translation>Ошибка синхронизации устройств.</translation>
+    </message>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="440"/>
+        <source>Error while toggling &apos;turn off on screensaver&apos;</source>
+        <translation>Ошибка при отключении хранителя экрана</translation>
+    </message>
+</context>
+<context>
+    <name>RazerGenieUi</name>
+    <message>
+        <location filename="../../ui/razergenie.ui" line="21"/>
+        <source>RazerGenie</source>
+        <translation>RazerGenie</translation>
+    </message>
+    <message>
+        <location filename="../../ui/razergenie.ui" line="68"/>
+        <source>Preferences</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../../ui/razergenie.ui" line="97"/>
+        <source>Sync devices</source>
+        <translation>Синхронизация устройств</translation>
+    </message>
+    <message>
+        <location filename="../../ui/razergenie.ui" line="104"/>
+        <source>Turn off on screensaver</source>
+        <translation>Отключить хранитель экрана</translation>
+    </message>
+</context>
+<context>
+    <name>RazerImageDownloader</name>
+    <message>
+        <location filename="../../src/razerimagedownloader.cpp" line="31"/>
+        <source>Image download disabled</source>
+        <translation>Загрузка картинок отключена</translation>
+    </message>
+    <message>
+        <location filename="../../src/razerimagedownloader.cpp" line="31"/>
+        <source>Image downloading is disabled. Visit the preferences to enable it.</source>
+        <translation>Загрузка картинок отключена. Включите в настрйоках, если нужно.</translation>
+    </message>
+    <message>
+        <location filename="../../src/razerimagedownloader.cpp" line="56"/>
+        <source>Network Error</source>
+        <translation>Ошибка сети</translation>
+    </message>
+</context>
+</TS>
