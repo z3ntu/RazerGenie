a9607d698e1963fd1b910055d03ac9b844eeaca2
NorwayFun <72336380+NorwayFun@users.noreply.github.com>
2023-10-10 06:45:55 +0200

    Add Georgian translation

---
+++ data/translations/ka.ts
@@ -0,0 +1,417 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka_GE">
+    <extra-po-header-language>ka</extra-po-header-language>
+    <extra-po-header-language_team>Georgian &lt;(nothing)&gt;</extra-po-header-language_team>
+    <extra-po-header-last_translator>Temuri Doghonadze &lt;temuri.doghonadze@gmail.com&gt;</extra-po-header-last_translator>
+    <extra-po-header-plural_forms>nplurals=2; plural=(n != 1);</extra-po-header-plural_forms>
+    <extra-po-header-po_revision_date></extra-po-header-po_revision_date>
+    <extra-po-header-pot_creation_date></extra-po-header-pot_creation_date>
+    <extra-po-header-project_id_version>RazerGenie</extra-po-header-project_id_version>
+    <extra-po-header-x_generator>Poedit 3.4</extra-po-header-x_generator>
+    <extra-po-headers>Project-Id-Version,POT-Creation-Date,PO-Revision-Date,Last-Translator,Language-Team,Language,MIME-Version,Content-Type,Content-Transfer-Encoding,Plural-Forms,X-Qt-Contexts,X-Generator</extra-po-headers>
+<context>
+    <name>CustomEditor</name>
+    <message>
+        <location filename="../../src/customeditor/customeditor.cpp" line="+18"/>
+        <source>RazerGenie - Custom Editor</source>
+        <translation>RazerGenie - მომხმარებლის რედაქტორი</translation>
+    </message>
+    <message>
+        <location line="+74"/>
+        <source>Set</source>
+        <translation>დაყენება</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Clear</source>
+        <translation>გაწმენდა</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Clear All</source>
+        <translation>ყველას გასუფთავება</translation>
+    </message>
+    <message>
+        <location line="+45"/>
+        <source>You are using a keyboard with a layout which is not known to the daemon. Please help us by visiting &lt;a href=&apos;https://github.com/openrazer/openrazer/wiki/Keyboard-layouts&apos;&gt;https://github.com/openrazer/openrazer/wiki/Keyboard-layouts&lt;/a&gt;. Using a fallback layout for now.</source>
+        <translation>იყენებთ კლავიატურას, რომლის განლაგებაც დემონმა არ იცის. შეგიძლიათ დაგვეხმაროთ. ეწვიეთ ვებგვერდს &lt;a href=&apos;https://github.com/openrazer/openrazer/wiki/Keyboard-layouts&apos;&gt;https://github.com/openrazer/openrazer/wiki/Keyboard-layouts&lt;/a&gt;. ჯერჯერობით ნაგულისხმევ განლაგებაზე გადავერთვები.</translation>
+    </message>
+    <message>
+        <location line="+190"/>
+        <source>The file %1.json, used for the custom editor failed to load: %2</source>
+        <translation>ფაილის, %1.json, რომელიც მომხმარებლის რედაქტორისთვის გამოიყენება, ჩატვირთვა ჩავარდა: %2</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>Error updating the lighting data.</source>
+        <translation>გამოკვეთის მონაცემების განახლების შეცდომა.</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceListWidget</name>
+    <message>
+        <location filename="../../src/devicelistwidget.cpp" line="+29"/>
+        <source>Downloading image...</source>
+        <translation>ასლის გადმოწერა....</translation>
+    </message>
+    <message>
+        <location line="+44"/>
+        <source>No image</source>
+        <translation>გამოსახულების გარეშე</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceWidget</name>
+    <message>
+        <location filename="../../src/devicewidget.cpp" line="+45"/>
+        <source>Lighting</source>
+        <translation>გამოკვეთა</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>Polling rate</source>
+        <translation>გამოკითხვის სიხშირე</translation>
+    </message>
+    <message>
+        <location line="+31"/>
+        <source>Open custom editor</source>
+        <translation>მომხმარებლის რედაქტორის გახსნა</translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <source>Serial number: %1</source>
+        <translation>სერიული ნომერი: %1</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Firmware version: %1</source>
+        <translation>მიკროკოდის ვერსია: %1</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Failed to set polling rate</source>
+        <translation>გამოკითხვის ის დაყენება ჩავარდა</translation>
+    </message>
+</context>
+<context>
+    <name>DpiComboBoxWidget</name>
+    <message>
+        <location filename="../../src/dpicomboboxwidget.cpp" line="+23"/>
+        <source>DPI</source>
+        <translation>DPI</translation>
+    </message>
+    <message>
+        <location line="+30"/>
+        <source>Failed to set DPI</source>
+        <translation>DPI-ის დაყენება ჩავარდა</translation>
+    </message>
+</context>
+<context>
+    <name>DpiSliderWidget</name>
+    <message>
+        <location filename="../../src/dpisliderwidget.cpp" line="+34"/>
+        <source>DPI</source>
+        <translation>DPI</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Lock X/Y</source>
+        <translation>X/Y-ის დაბლოკვა</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>DPI X</source>
+        <translation>DPI X</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>DPI Y</source>
+        <translation>DPI Y</translation>
+    </message>
+    <message>
+        <location line="+113"/>
+        <source>Failed to set dpi</source>
+        <translation>DPI-ის დაყენება ჩავარდა</translation>
+    </message>
+</context>
+<context>
+    <name>LedWidget</name>
+    <message>
+        <location filename="../../src/ledwidget.cpp" line="+24"/>
+        <source>Lighting %1</source>
+        <translation>გამოკვეთა %1</translation>
+    </message>
+    <message>
+        <location line="+44"/>
+        <source>Brightness</source>
+        <translation>სიკაშკაშე</translation>
+    </message>
+    <message>
+        <location line="+46"/>
+        <source>Left</source>
+        <translation>მარცხენა</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Right</source>
+        <translation>მარჯვენა</translation>
+    </message>
+    <message>
+        <location line="+118"/>
+        <source>Failed to change brightness</source>
+        <translation>სიკაშკაშის შეცვლა ჩავარდა</translation>
+    </message>
+    <message>
+        <location line="+72"/>
+        <source>Failed to change effect</source>
+        <translation>ეფექტის შეცვლა ჩავარდა</translation>
+    </message>
+</context>
+<context>
+    <name>Preferences</name>
+    <message>
+        <location filename="../../src/preferences/preferences.cpp" line="+20"/>
+        <source>RazerGenie - Preferences</source>
+        <translation>RazerGenie - მორგება</translation>
+    </message>
+    <message>
+        <location line="+25"/>
+        <source>About</source>
+        <translation>შესახებ</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>RazerGenie Version:</source>
+        <translation>RazerGenie-ის ვერსია:</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>OpenRazer Daemon Version:</source>
+        <translation>OpenRazer-ის დემონის ვერსია:</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>General</source>
+        <translation>ზოგადი</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Download device images</source>
+        <translation>მოწყობილობის ასლების გადმოწერა</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Device images:</source>
+        <translation>მოწყობილობის ასლები:</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>For displaying device images, RazerGenie downloads the image behind the URL specified for a device in the OpenRazer daemon source code. This will only be done for devices that are connected to the PC and only once, as the images are cached locally. For reviewing, what information Razer might collect with these connections, please consult the &lt;a href=&quot;https://www.razer.com/legal/privacy-policy&quot;&gt;Razer Privacy Policy&lt;/a&gt;.</source>
+        <translation>მოწყობილობების ასლების საჩვენებლად, RazerGenie იწერს ასლს, რომლის ბმულიც, მოწყობილობისთვის, OpenRazer-ის დემონის კოდშია მითითებული. ეს, მხოლოდ იმ მოწყობილობებისთვის მოხდება, რომლებიც მიერთებულია კომპიუტერზე და მხოლოდ ერთხელ, რადგან ასლები ლოკალურ კეშში ინახება. იმის სანახავად, რა ინფორმაციის მიღება შეუძლია Razer-ს ამის შედეგად, იხილეთ &lt;a href=&quot;https://www.razer.com/legal/privacy-policy&quot;&gt;Razer-ის კონფიდენციალობის პოლიტიკა&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Don&apos;t ask to auto-start daemon on startup</source>
+        <translation>დემონი ჩატვირთვისას ავტომატურად არ გაეშვას</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Daemon auto-start:</source>
+        <translation>დემონის ავტომატური გაშვება:</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Please restart the application for the switch to take effect.</source>
+        <translation>ცვლილებების ძალაში შესასვლელად საჭიროა აპლიკაციის გადატვირთვა.</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Daemon backend:</source>
+        <translation>დემონის უკანაბოლო:</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <location filename="../../src/util.cpp" line="+12"/>
+        <source>Error!</source>
+        <translation>შეცდომა!</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Information!</source>
+        <translation>ინფორმაცია!</translation>
+    </message>
+</context>
+<context>
+    <name>RazerGenie</name>
+    <message>
+        <location filename="../../src/razergenie.cpp" line="+51"/>
+        <source>The OpenRazer daemon is not installed</source>
+        <translation>OpenRazer-ის დემონი დაყენებული არაა</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>The daemon is not installed or the version installed is too old. Please follow the installation instructions on the website!
+
+If you are running RazerGenie as a flatpak, you will still have to install OpenRazer outside of flatpak from a distribution package.</source>
+        <translation>დემონი დაყენებული არაა ან ვერსია, რომელიც დაყენებულია, მეტისმეტად ძველია. მიჰყევით დაყენების ინსტრუქციებს ვებგვერდზე!
+
+თუ აპირებთ, RazerGenie flatpak-ის სახით გამოიყენოთ, OpenRazer-ის დაყენება, დისტრიბუტივის პაკეტით, მაინც მოგიწევთ.</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Open OpenRazer website</source>
+        <translation>OpenRazer-ის ვებგვერდის გახსნა</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <location line="+16"/>
+        <location line="+18"/>
+        <source>Open settings</source>
+        <translation>პარამეტრების გახსნა</translation>
+    </message>
+    <message>
+        <location line="-20"/>
+        <source>The OpenRazer daemon is not available.</source>
+        <translation>OpenRazer-ის დემონი ხელმისაწვდომი არაა.</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>The OpenRazer daemon is not started and you are not using systemd as your init system.
+You have to either start the daemon manually every time you log in or set up another method of autostarting the daemon.
+
+Please consult the documentation for details.</source>
+        <translation>OpenRazer-ის დემონი გაშვებული არაა და თქვენ systemd-ს არ იყენებთ.
+დემონი ან ხელით უნდა გაუშვათ ყოველ ჯერზე, ან მისი ავტომატური გაშვება უზრუნველყოთ.
+
+დეტალებისთვის იხილეთ დოკუმენტაცია.</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>The OpenRazer daemon is currently not available. The status output is below.</source>
+        <translation>OpenRazer-ის დემონი ამჟამად ხელმისაწვდომი არაა. სტატუსის გამოტანა იხილეთ ქვემოთ.</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>If you think, there&apos;s a bug, you can report an issue on GitHub:</source>
+        <translation>თუ ფიქრობთ, რომ ეს შეცდომაა, მოგვწერეთ ამის შესახებ GitHub-ზე:</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <location line="+318"/>
+        <location line="+8"/>
+        <source>Report issue</source>
+        <translation>პრობლემის შეტყობინება</translation>
+    </message>
+    <message>
+        <location line="-302"/>
+        <source>The OpenRazer daemon is not set to auto-start. Click &quot;Enable&quot; to use the full potential of the daemon right after login.</source>
+        <translation>OpenRazer-ის დემონ ავტომატურად არ ეშვება. დააწკაპუნეთ &apos;ჩართვაზე&apos;, რათა დემონის სრული ძალა სისტემაში შესვლის შემდეგვე იგრძნოთ.</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Enable</source>
+        <translation>ჩართვა</translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <source>Daemon version: %1</source>
+        <translation>დემონის ვერსია: %1</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>The D-Bus connection was re-established.</source>
+        <translation>D-Bus-თან კავშირი თავიდან დამყარდა.</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>The D-Bus connection was lost, which probably means that the daemon has crashed.</source>
+        <translation>D-Bus-თან მიერთება დაიკარგა, რაც, ალბათ ნიშნავს, რომ დემონი ავარიულად დასრულდა.</translation>
+    </message>
+    <message>
+        <location line="+228"/>
+        <source>No device was detected</source>
+        <translation>მოწყობილობა ვერ ვიპოვე</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>The OpenRazer daemon didn&apos;t detect a device that is supported.
+This could also be caused due to a misconfiguration of this PC.</source>
+        <translation>OpenRazer-ის დემონმა ვერ აღმოაჩინა მოწყობილობა, რომელსაც ის მხარს უჭერდა.
+ესეც, შეიძლება, ამ კომპიუტერის არასწორი კონფიგურაციის ბრალი იყოს.</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Open supported devices</source>
+        <translation>მხარდაჭერილი მოწყობილობების გახსნა</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>The daemon didn&apos;t detect a device that is connected</source>
+        <translation>დემონმა მიერთებული მოწყობილობა ვერ იპოვა</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Linux detected connected devices but the daemon didn&apos;t. This could be either due to a permission problem or a kernel module problem.</source>
+        <translation>Linux-მა აღმოაჩინა მიერთებული მოწყობილობები, მაგრამ დემონმა - ვერ. ეს წვდომების ან ბირთვის მოდულის პრობლემაა.</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Open troubleshooting page</source>
+        <translation>პროლემების გადაჭრის გვერდის გახნა</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>Error while syncing devices.</source>
+        <translation>შეცდომა მოწყობილობების სინქრონიზაციისას.</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Error while toggling &apos;turn off on screensaver&apos;</source>
+        <translation>შეცდომა &apos;ეკრანს დამცველის ჩართვა/გამორთვის&apos; გადართვისას</translation>
+    </message>
+</context>
+<context>
+    <name>RazerGenieUi</name>
+    <message>
+        <location filename="../../ui/razergenie.ui" line="+21"/>
+        <source>RazerGenie</source>
+        <translation>RazerGenie</translation>
+    </message>
+    <message>
+        <location line="+62"/>
+        <source>Preferences</source>
+        <translation>მორგება</translation>
+    </message>
+    <message>
+        <location line="+42"/>
+        <source>Sync devices</source>
+        <translation>მოწყ-ების სინქრონიზაცია</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Turn off on screensaver</source>
+        <translation>ეკრანის დამცველის გამორთვა ან ჩართვა</translation>
+    </message>
+</context>
+<context>
+    <name>RazerImageDownloader</name>
+    <message>
+        <location filename="../../src/razerimagedownloader.cpp" line="+31"/>
+        <source>Image download disabled</source>
+        <translation>ასლების გადმოწერა გათიშულია</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Image downloading is disabled. Visit the preferences to enable it.</source>
+        <translation>ასლების გადმოწერა გათიშულია. ჩასართავად, მორგების ფანჯარაზე გადადით.</translation>
+    </message>
+    <message>
+        <location line="+25"/>
+        <source>Network Error</source>
+        <translation>ქსელის შეცდომა</translation>
+    </message>
+</context>
+</TS>
